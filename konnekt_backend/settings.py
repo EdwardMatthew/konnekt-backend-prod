07bc4eec613217ab69048fa7296466fb047f86ad
Edward Matthew <edwardmk2704@gmail.com>
2022-06-13 22:43:54 +0700

    files for deployment

--- konnekt_backend/settings.py
+++ konnekt_backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r24^ub01k^$n-iuro%d2!^p(*#p*^7ag^y=+z#-%z6t5017kxu'
+#SECRET_KEY = 'django-insecure-r24^ub01k^$n-iuro%d2!^p(*#p*^7ag^y=+z#-%z6t5017kxu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
