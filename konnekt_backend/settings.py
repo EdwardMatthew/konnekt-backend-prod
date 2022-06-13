ee96930548a4b72246775896125045ac8a5c2f0c
Edward Matthew <edwardmk2704@gmail.com>
2022-06-14 01:04:32 +0700

    refactored some code

--- konnekt_backend/settings.py
+++ konnekt_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,6 +137,9 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DATABASES = {}
+DATABASES["default"] = dj_database_url(conn_max_age=600)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
